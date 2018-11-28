aacde65d9e1aacfd97478495c1f75c04ede46f0c
nicyann <yannito009@gmail.com>
2018-11-28 11:10:02 +0100

    initialisation liens affichages objets trouvés

--- src/Controller/TraobjectController.php
+++ src/Controller/TraobjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\State;
 use App\Entity\Traobject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,5 +30,21 @@ class TraobjectController extends AbstractController
         return $this->render('traobject/show.html.twig', [
             'traobj' => $traobj
         ]);
+    }
+    
+    /**
+     * @Route ("/found", name="foundobjet")
+     *
+     */
+    public function ObjectFound()
+    {
+        $stateTrouve = $this->getDoctrine()->getRepository(State::class)->findOneBy(["label" => State::FOUND]);
+        $trouvobjects = $this->getDoctrine()->getRepository(Traobject::class)->findBy(array('state' => $stateTrouve));
+        
+        return $this->render('traobject/trouvobjet.html.twig', [
+            'trouvobjects' => $trouvobjects
+        ]);
+        
+        
     }
 }
